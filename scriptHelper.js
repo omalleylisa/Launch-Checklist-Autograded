63328d73a17d89bdd20a1bd749a79148a31ae5ff
Lisa O'Malley <lisaomalley@Lisas-MBP.lan>
2024-03-14 18:14:28 -0500

    slight changes

--- scriptHelper.js
+++ scriptHelper.js
@@ -20,7 +20,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput) {
     if (testInput === "") {
         return "Empty";
-    } else if (isNaN(Number(testInput))) {
+    } else if (isNaN(testInput)) {
         return "Not a Number";
     } else {
         return "Is a Number";
